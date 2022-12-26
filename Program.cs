243b1c6242601733c1a25db6ee386e6d6bb8a7da
mohamedYasser55 <121432150+mohamedYasser55@users.noreply.github.com>
2022-12-26 13:51:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, num, i;
+            Console.WriteLine("min:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("max:");
+            n2 = int.Parse(Console.ReadLine());
+            for (num = n1; num < n2; num++)
+            {
+                decimal sum = 0;
+                for (i = 1; i < num; i++)
+                {
+                    if (num % i == 0)
+                        sum = sum + i;
+                }
+                if (sum == num)
+                {
+                }
+
+
+
+        }
+    }
+}
